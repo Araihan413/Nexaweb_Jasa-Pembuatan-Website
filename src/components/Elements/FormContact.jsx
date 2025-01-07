dcdacc4648079aa050054c22c7fef09644775add
Araihan413 <araihannajah03@gmail.com>
2025-01-07 17:00:28 +0700

    menyelesaikan fitur kirim email di halaman kontak

--- src/components/Elements/FormContact.jsx
+++ src/components/Elements/FormContact.jsx
@@ -1,26 +1,26 @@
 export const FormContact = (props) => {
-  const { id, children, type, placeholder, style = '' } = props;
+  const { id, children, type, placeholder, style = '', required } = props;
   return (
     <>
       <div className={` ${style} px-2 flex flex-col gap-2 pb-5`}>
-        <label className="font-semibold font-subJudul" htmlFor={id}>
+        <label className={`font-semibold font-subJudul ${required ? 'after:content-["*"] after:text-sm after:text-red-400 after:pl-1' : ''}`} htmlFor={id}>
           {children}
         </label>
-        <input id={id} name={id} className="rounded-md py-2 px-2" type={type} placeholder={placeholder} />
+        <input required={required} id={id} name={id} className="rounded-md py-2 px-2" type={type} placeholder={placeholder} />
       </div>
     </>
   );
 };
 
 export const FormTextarea = (props) => {
-  const { id, children, placeholder, style = '' } = props;
+  const { id, children, placeholder, style = '', required } = props;
   return (
     <>
       <div className={` ${style} px-2 flex flex-col gap-2 pb-5`}>
-        <label className="font-semibold font-subJudul" htmlFor={id}>
+        <label className={`font-semibold font-subJudul ${required ? 'after:content-["*"] after:text-sm after:text-red-400 after:pl-1' : ''}`} htmlFor={id}>
           {children}
         </label>
-        <textarea rows="2" id={id} name={id} className="rounded-md py-2 px-2" placeholder={placeholder} />
+        <textarea required={required} rows="2" id={id} name={id} className="rounded-md py-2 px-2" placeholder={placeholder} />
       </div>
     </>
   );
--- src/components/Fragments/Contact.jsx
+++ src/components/Fragments/Contact.jsx
@@ -2,8 +2,12 @@ import contactMobile from '../../assets/figure/contact_mobile.svg';
 import { FormContact, FormTextarea } from '../Elements/FormContact';
 import { motion } from 'framer-motion';
 import Button from '../Elements/Button';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Contact = () => {
+
   const fromRight = {
     isView: { opacity: 1, x: 0, transition: { duration: 0.5 } },
     isHidden: { opacity: 0, x: 50 },
@@ -16,10 +20,31 @@ const Contact = () => {
     isView: { opacity: 1, y: 0, transition: { duration: 0.5 } },
     isHidden: { opacity: 0, y: 50 },
   };
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_nxanxot", "template_mgbhf58", form.current, {
+      publicKey: 'z71v_orlXjAYVZV4b',
+    })
+      .then(
+        () => {
+          toast.success('Pesan berhasil dikirim! 😊', { position: 'top-right', autoClose: 8000 });
+          form.current.reset();
+
+        },
+        (error) => {
+          toast.error('Gagal mengirim pesan! 😔', { position: 'top-right', autoClose: 8000 });
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
 
   return (
     <>
       <div>
+        <ToastContainer />
         <div className="flex flex-col justify-center items-center text-center pb-8">
           <motion.h1 variants={fromButton} whileInView="isView" initial="isHidden" viewport={{ once: true }} className="font-judul text-4xl font-semibold mb-2">Hubungi Kami</motion.h1>
           <motion.p variants={fromButton} whileInView="isView" initial="isHidden" viewport={{ once: true }} className='font-paragraf text-lg font-semibold w-full md:w-9/12 lg:w-1/2'>Kami selalu terbuka untuk mendengar dari Anda! Baik itu saran, kritik, kerja sama proyek, atau pertanyaan seputar layanan kami, Nexaweb siap membantu Anda dengan tulus.</motion.p>
@@ -27,29 +52,17 @@ const Contact = () => {
         <div className="w-full justify-center items-end grid lg:grid-cols-2 grid-cols-1 py-5 gap-10 lg:gap-0">
           <div className="w-full flex justify-center">
             <motion.div variants={fromLeft} whileInView="isView" initial="isHidden" viewport={{ once: true }} className="bg-slate-200/40 backdrop-blur rounded-xl col-span-1 w-11/12 shadow-lg max-w-[700px]">
-              <form action="" className="grid grid-cols-1 md:grid-cols-2 p-2 pt-5">
-                <FormContact id="nama" type="text" placeholder="Nama Lengkap" style="col-span-1">
+              <form className="grid grid-cols-1 p-2 pt-5" ref={form} onSubmit={sendEmail} method="POST">
+                <FormContact id="from_name" type="text" placeholder="Nama Lengkap" style="col-span-1" required={true}>
                   Nama Lengkap
                 </FormContact>
-                <FormContact id="perusahaan" type="text" placeholder="Perusahaan" style="col-span-1">
-                  Perusahaan
-                </FormContact>
-
-                <FormContact id="telepon" type="text" placeholder="Telepon / WhatsApp" style="col-span-1">
-                  Telepon / WhatsApp
-                </FormContact>
-                <FormContact id="email" type="email" placeholder="Email" style="col-span-1">
+                <FormContact id="from_email" type="email" placeholder="Email" style="col-span-1" required={true}>
                   Email
                 </FormContact>
-
-                <FormContact id="subjek" type="text" placeholder="Subjek" style="col-span-1 md:col-span-2">
-                  Subjek
-                </FormContact>
-
-                <FormTextarea id="Pesan" placeholder="Pesan Anda" style="col-span-1 md:col-span-2">
+                <FormTextarea id="message" placeholder="Pesan Anda" style="col-span-1" required={true}>
                   Pesan
                 </FormTextarea>
-                <div className='col-span-1 md:col-span-2 px-8 pb-5'>
+                <div className='col-span-1 px-8 pb-5'>
                   <Button type="submit" warna="bg-blue-300" style="w-full">Kirim</Button>
                 </div>
               </form>
